6a9a17ad394620bff6904773cbaf889c8df0423a
Winton Welsh <win@sumapp.com>
2008-08-17 18:22:22 -0500

    Gem working

--- captcha.gemspec
+++ captcha.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name    = 'captcha'
-  s.version = '1.0.2'
+  s.version = '1.0.3'
   s.date    = '2008-08-16'
   
   s.summary     = "A simple captcha generator for Rails"
--- lib/captcha.rb
+++ lib/captcha.rb
@@ -44,3 +44,16 @@ class Captcha
   end
 
 end
+
+to = "public/images/captchas"
+unless File.exists?(to)
+  FileUtils.mkdir_p to
+  (0..99).each do |x|
+    c = Captcha.new(6)
+    File.open("#{to}/#{c.code}.jpg", 'w') do |f|
+      f << c.code_image
+    end
+  end
+end
+
+ActionController::Base.send :include, CaptchaActions
